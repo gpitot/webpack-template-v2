0cf53287588a7d3ea9f145a573679f21b712c73d
Guillaume Pitot <gpitot@nine.com.au>
2019-07-08 14:57:10 +1000

    new comp admin check for get results

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import style from "./sass/index.scss";
+import fonts from "./sass/fonts.scss";
 
 import CompApi from './js/comp-api';
 
 
-new CompApi(document.getElementById('parent'))
+const comp = new CompApi(document.getElementById('parent'));
+comp.renderForm();
+
+
+comp.getResults(39).then(res => console.log(res));
