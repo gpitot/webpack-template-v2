7fbc188195038bd323b3b2c9d96669ff8a53409b
Guillaume Pitot <gpitot@nine.com.au>
2019-07-08 14:57:20 +1000

    config different, more like api

--- src/js/comp-api/config.js
+++ src/js/comp-api/config.js
@@ -10,74 +10,73 @@ const phases = [
     },
     {
         startDate : new Date(startCompTime + (weekInSeconds * 1)), 
-        phase : 40
+        phase : 42
     },
     {
         startDate : new Date(startCompTime + (weekInSeconds * 2)), 
-        phase : 41
+        phase : 42
     }
 ];
 
 const endpoints = {
     url : 'https://staging.competition.nine.com.au/enter-phase/',
     entriesUrl: 'https://staging.competition.nine.com.au/entries/',
+}
 
-    formConfig : [
+
+const form = {
+    firstName : {
+        label : 'First name',
+        value : '',
+        inputType : 'text'
+    },
+    lastName : {
+        label : 'Last name',
+        value : '',
+        inputType : 'text'
+    },
+    email : {
+        label : 'Email',
+        value : '',
+        inputType : 'text'
+    },
+    phone : {
+        label : 'Phone Number',
+        value : '',
+        inputType : 'number'
+    },
+    state : {
+        label : 'State',
+        value : '',
+        inputType : 'text'
+    },
+    postcode : {
+        label : 'Postcode',
+        value : '',
+        inputType : 'number'
+    },
+    newsletter : {
+        label : 'Yes, I consent to receive information on Suncorp Products.',
+        value : '',
+        inputType : 'checkbox'
+    },
+    customFields : [
         {
-            label : 'First name',
-            name : 'firstName',
-            value : '',
-            type : 'text',
-        },
-        {
-            label : 'Last name',
-            name : 'lastName',
-            value : '',
-            type : 'text'
-        },
-        {
-            label : 'Email',
-            name : 'email',
-            value : '',
-            type : 'email'
-        },
-        {
-            label : 'Phone Number',
-            name : 'phone',
-            value : '',
-            type : 'number'
-        },
-        {
-            label : 'State',
-            name : 'state',
-            value : '',
-            type : 'text'
-        },
-        {
-            label : 'Postcode',
-            name : 'postcode',
-            value : '',
-            type : 'number'
-        },
-        {
-            label : 'Yes, I consent to receive information on Suncorp Products.',
-            name : 'newsletter',
-            value : '',
-            type : 'checkbox'
-        },
-        {
+            id :87,
+            type : 'bool',
             label : 'Yes, I am happy for Suncorp to use my above details for marketing and understand I can opt out at any time.',
             name : 'optin',
             value : '',
-            type : 'checkbox'
-        },
-
-
+            inputType : 'checkbox',
+            customClass : 'optin-row',
+        }
     ]
 }
+    
 
 
 export {
     phases,
-    endpoints
+    endpoints,
+    form
 }
