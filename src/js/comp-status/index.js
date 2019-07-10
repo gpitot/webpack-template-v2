b561b96f1b9f431635776571e900daf4f5ca6b0f
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2019-07-10 15:18:43 +1000

    confirmation page

--- src/js/comp-status/index.js
+++ src/js/comp-status/index.js
@@ -1,14 +1,19 @@
-// check what the state is of the URL - submitted? 
-// how to get query parameter from a URL
+// comp entry
+const closeDate = new Date('Aug 10 2019');
+let today = new Date();
 
-
-// if submitted, display none on first block and show thankyou block
-
-// if state =?close, hide firstblock then 
 function checkStatus() {
+    // if currentDate is greater than closed date, do same as below
+    if (today > closeDate) {
+        const blockRight = document.querySelector('.block__right');
+        blockRight.style.display = 'none';
+        const closePage = document.querySelector('.block__right--closed');
+        closePage.style.display = 'block';
+        return;
+    }
     const urlParams = new URLSearchParams(window.location.search);
     const state = urlParams.get('state');
-    if (state ==='submitted') {
+    if (state === 'submitted') {
         const thankYouPage = document.querySelector('.block__right--thankyou');
         thankYouPage.style.display = 'block';
         const blockRight = document.querySelector('.block__right');
